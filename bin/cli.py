ce511c99b17e0fd3c4b8f065a82882d9fe3b9419
Michael Scales <mscales@mindwireless.com>
2017-02-09 16:41:01 -0600

    added schedule to record absent/late agent info

--- bin/cli.py
+++ bin/cli.py
@@ -6,91 +6,97 @@
 
 # -*- coding: utf-8 -*-
 import traceback
-from re import search
-import pyexcel as pe
-from pyexcel import Sheet, get_book
-from pyexcel.book import Book
+from pyexcel import Sheet, get_book, get_sheet
 from os.path import dirname, abspath, join
-from collections import OrderedDict
-from utilities.utilities import get_sec, header_filter, safe_div
+from types import MethodType
+from utilities.utilities import safe_div, create_schedule, read_time_card, open_wb, read_feature_card
 
 
-# def get_my_book(**keywords):
-#     book_stream = _get_book(**keywords)
-#     my_book = CustomBook(book_stream.to_dict(),
-#                          filename=book_stream.filename,
-#                          path=book_stream.path)
-#     return my_book
-#
-#
-# class CustomBook(Book):
-#     def __hash__(self):
-#         return hash((self.name, self.location))
-#
-#     def __eq__(self, other):
-#         return (self.name, self.location) == (other.name, other.location)
-#
-#     def __ne__(self, other):
-#         return not (self == other)
+VALID_DAYS_IN_MONTH = 21
+
+
+def in_schedule(schedule):
+    for k, v in schedule.items():
+        yield(
+            (
+                r'{0} {1}({2})'.format(v.f_name, v.l_name, v.ext), v
+            )
+        )
 
 
 def main():
     directory = join(dirname(dirname(abspath(__file__))), 'raw_files')
+
     reports = {
-        'agent_time_card': get_book(file_name=join(directory, 'Agent_Time_Card.xlsx')),
-        'feature_trace': get_book(file_name=join(directory, 'Agent_Realtime_Feature_Trace.xlsx'))
+        'agent_time_card': open_wb(get_book(file_name=join(directory, 'Agent_Time_Card.xlsx'))),
+        'feature_trace': open_wb(get_book(file_name=join(directory, 'Agent_Realtime_Feature_Trace.xlsx')))
     }
-    summary = OrderedDict()
-    reports[reports['feature_trace']] = ('Feature Type', 'Do Not Disturb')
+
+    schedule = create_schedule(
+        get_sheet(file_name=join(r'M:/Help Desk/', 'Schedules for OPS.xlsx'),
+                  name_columns_by_row=0,
+                  name_rows_by_column=0)
+    )
+
+    # Bind read function to respective reports
+    check_time_card = MethodType(read_time_card, reports['agent_time_card'])
+    check_feature_card = MethodType(read_feature_card, reports['feature_trace'])
+
+    output = Sheet(colnames=['', '% Avail', 'Absences', 'Late'])
     try:
-        wb_filter = pe.filters.RowFilter(header_filter)
-        for rpt_name, rpt in reports.items():
-            if isinstance(rpt, Book):
-                rpt.remove_sheet('Summary')
-                r_filter = (
-                    {
-                        'filter_col': reports[rpt][0],
-                        'filter_val': reports[rpt][1]
-                    } if isinstance(reports.get(rpt, None), tuple)
-                    else None
-                )
-                for sheet in rpt:
-                    sheet.filter(wb_filter)
-                    sheet.name_columns_by_row(0)
-                    sheet.name_rows_by_column(0)
-                    for row_name in sheet.rownames:
-                        try:
-                            if r_filter:
-                                if sheet[row_name, r_filter['filter_col']] == r_filter['filter_val']:
-                                    cell_value = sheet[row_name, 'Duration']
-                                else:
-                                    raise ValueError()
-                            else:
-                                cell_value = sheet[row_name, 'Duration']
-                        except ValueError:
-                            pass
-                        else:
-                            try:
-                                summary[sheet.name][rpt_name] += get_sec(cell_value)
-                            except KeyError:
-                                summary[sheet.name] = {
-                                    **summary.get(sheet.name, {}),
-                                    rpt_name: get_sec(cell_value)
-                                }
-        # print(
-        #     '\n'.join(['{agent}\n{values}'.format(agent=k, values=v) for k, v in summary.items()])
-        # )
-        output = Sheet(colnames=['', '% Avail'])
-        for agent, col_data in summary.items():
-            if 'feature_trace' in col_data.keys():
-                output.row += [
-                    '{row_name}'.format(
-                        row_name=int(search(r"([0-9]+)", agent).group(0))
-                    ),
-                    '{val:.1%}'.format(
-                        val=safe_div(col_data['feature_trace'], col_data['agent_time_card'])
-                    )
-                ]
+        for sheet_name, emp_data in in_schedule(schedule):
+            tc_data = check_time_card(sheet_name, emp_data)
+            dnd_time = check_feature_card(sheet_name)
+            output.row += [
+                '{row_name}'.format(
+                    row_name=emp_data.ext
+                ),
+                '{val:.1%}'.format(
+                    val=safe_div(dnd_time, tc_data['Duration'])
+                ),
+                VALID_DAYS_IN_MONTH - tc_data['Log Events'],
+                tc_data['Late']
+            ]
+        # for rpt_name, rpt in reports.items():
+        #     if isinstance(rpt, Book):
+        #         r_filter = (
+        #             {
+        #                 'filter_col': reports[rpt][0],
+        #                 'filter_val': reports[rpt][1]
+        #             } if isinstance(reports.get(rpt, None), tuple)
+        #             else None
+        #         )
+        #         for sheet in rpt:
+        #             for row_name in sheet.rownames:
+        #                 try:
+        #                     if r_filter:
+        #                         if sheet[row_name, r_filter['filter_col']] == r_filter['filter_val']:
+        #                             cell_value = sheet[row_name, 'Duration']
+        #                         else:
+        #                             raise ValueError()
+        #                     else:
+        #                         cell_value = sheet[row_name, 'Duration']
+        #                 except ValueError:
+        #                     pass
+        #                 else:
+        #                     try:
+        #                         summary[sheet.name][rpt_name] += get_sec(cell_value)
+        #                     except KeyError:
+        #                         summary[sheet.name] = {
+        #                             **summary.get(sheet.name, {}),
+        #                             rpt_name: get_sec(cell_value)
+        #                         }
+
+        # for agent, col_data in summary.items():
+        #     if 'feature_trace' in col_data.keys():
+        #         output.row += [
+        #             '{row_name}'.format(
+        #                 row_name=int(search(r"([0-9]+)", agent).group(0))
+        #             ),
+        #             '{val:.1%}'.format(
+        #                 val=safe_div(col_data['feature_trace'], col_data['agent_time_card'])
+        #             )
+        #         ]
         else:
             output.name_rows_by_column(0)
             print(output)
--- utilities/utilities.py
+++ utilities/utilities.py
@@ -1,4 +1,7 @@
 import re
+from datetime import datetime, timedelta
+from collections import namedtuple
+from dateutil.parser import parse
 
 
 def get_sec(time_string):  # returns time provided as as string in hours, minutes, sec
@@ -13,14 +16,108 @@ def convert_sec(h, m, s):  # Converts hours, minutes, sec to seconds
     return (3600 * int(h)) + (60 * int(m)) + int(s)
 
 
-def header_filter(row):
+def header_filter(row_index, row):
     corner_case = re.split('\(| - ', row[0])
     bad_word = corner_case[0].split(' ')[0] not in ('Feature', 'Call', 'Event')
     return True if len(corner_case) > 1 else bad_word
 
 
 def safe_div(n, d):
+    rtn = 0
     try:
-        return n / d
+        rtn = n / d
     except ZeroDivisionError:
-        return 0
+        pass
+    return 1 if rtn > 1 else rtn
+
+
+def date_tuple_factory(date_string):
+        dt = namedtuple('date', 'start end')
+        try:
+            (raw_start, raw_end) = split_str(date_string)
+        except ValueError:
+            return_dt = None
+        else:
+            dt_start = datetime.strptime(raw_start, '%H:%M').time()
+            dt_end = datetime.strptime(raw_end, '%H:%M').time()
+            return_dt = dt(start=dt_start, end=dt_end)
+        return return_dt
+
+
+def split_str(t_string):
+    return t_string.split('-')
+
+
+def safe_parse(dt=None):
+    try:
+        return parse(dt)
+    except ValueError:
+        # print('Could not parse date_time: {dt}'.format(dt=dt))
+        return None
+
+
+def create_schedule(data):
+    return_dict = {}
+    new_schedule = namedtuple('this_emp', 'Monday Tuesday Wednesday Thursday Friday '
+                                          'Saturday Sunday f_name l_name data ext')
+    new_schedule.__new__.__defaults__ = (None,) * len(new_schedule._fields)
+    for emp in data.rownames:
+        emp_schedule = new_schedule(f_name=data[emp, 'First'],
+                                    l_name=data[emp, 'Last'],
+                                    ext=int(emp),
+                                    Monday=date_tuple_factory(data[emp, 'Monday']),
+                                    Tuesday=date_tuple_factory(data[emp, 'Tuesday']),
+                                    Wednesday=date_tuple_factory(data[emp, 'Wednesday']),
+                                    Thursday=date_tuple_factory(data[emp, 'Thursday']),
+                                    Friday=date_tuple_factory(data[emp, 'Friday']),
+                                    Saturday=date_tuple_factory(data[emp, 'Saturday']),
+                                    Sunday=date_tuple_factory(data[emp, 'Sunday']))
+        return_dict[emp] = emp_schedule
+    return return_dict
+
+
+def read_time_card(time_card, sheet_name, emp_data):
+    rtn_data = {
+        'Late': 0,
+        'Duration': 0,
+        'Log Events': []
+    }
+    sheet = time_card[sheet_name]
+    for row_name in sheet.rownames:
+        # print(row_name)
+        start_time = safe_parse(dt=sheet[row_name, "Logged In"])
+        end_time = safe_parse(dt=sheet[row_name, "Logged Out"])
+        if start_time.date() == end_time.date() if start_time and end_time else start_time:  # Excludes roll over days
+            try:
+                shift_start = emp_data[start_time.weekday()].start  # checks if the agent is scheduled this date
+            except AttributeError:
+                pass
+                # print(start_time)
+                # print('**Extra Day**')
+            else:
+                rtn_data['Duration'] += get_sec(sheet[row_name, "Duration"])  # Any duration on a valid day counts.
+                if start_time.date() not in rtn_data['Log Events']:  # "hashes" date - counts ea. login one time
+                    rtn_data['Log Events'].append(start_time.date())
+                    grace_time = datetime.combine(start_time.date(), shift_start) + timedelta(minutes=5)
+                    if start_time > grace_time:  # Can only be late once per non-absent day
+                        rtn_data['Late'] += 1
+    rtn_data['Log Events'] = len(rtn_data['Log Events'])
+    return rtn_data
+
+
+def read_feature_card(feature_card, sheet_name):
+    rtn_data = 0
+    sheet = feature_card[sheet_name]
+    for row_name in sheet.rownames:
+        if sheet[row_name, 'Feature Type'] == 'Do Not Disturb':
+            rtn_data += get_sec(sheet[row_name, 'Duration'])
+    return rtn_data
+
+
+def open_wb(wb):
+    wb.remove_sheet('Summary')
+    for sheet in wb:
+        del sheet.row[header_filter]
+        sheet.name_columns_by_row(0)
+        sheet.name_rows_by_column(0)
+    return wb
